09161147c70748b6a662c91d20bb48ddb820835b
ChenyuGAO-CS <chenyugao@mail.nwpu.edu.cn>
2024-04-10 21:40:39 +0100

    Update script for violin plot.

--- violin_plot.R
+++ violin_plot.R
@@ -1,7 +1,7 @@
 library("ggpubr")
 
-p <- read.csv("all_violin_plot.csv")
-p$Category <- factor(p$Category, levels = c(" MAMA", " FaTr"," MuTr", " VaTr", " Hu"))
+p <- read.csv("scores_all_violin.csv")
+p$Category <- factor(p$Category, levels = c("SystemX", "SystemAT"))
 p <- ggviolin(p, x = "Category", y = "Rating", orientation = "horiz")
 p <- add_summary(p, fun = "median_q1q3", size = 0.3)
 p <- facet(p +
@@ -11,8 +11,8 @@ p <- facet(p +
              geom_hline(yintercept = 6, linetype = "dotted", size = 0.5, alpha = 0.4) +
              scale_y_discrete(limits = c("1", "2", "3", "4", "5", "6", "7")),
            scales = "free",
-           panel.labs = list(Aspect = c("Vs", "Sc", "Si", "Cr", "Mq", "Ws", "Turing"), Part = c("POP909-TV", "VGMIDI-TV")),
+           panel.labs = list(Aspect = c("Remuneration", "PayPreparedness", "Transformative", "Speed", "Convenience", "Ethics", "Legal", "TechSupport", "Rethink"), Part = c("CPI")),
            facet.by = c("Part", "Aspect"))
 p
 ggarrange(p) %>%
-  ggexport(width = 4800, height = 1800, res = 400, filename = "ratings.png")
+  ggexport(width = 4800, height = 800, res = 400, filename = "ratings.png")
